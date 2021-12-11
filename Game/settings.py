b499ba86223a31b89dd38f06f577cae0b9d9cb88
Matheus Silva <69468446+silvamva@users.noreply.github.com>
2021-12-11 02:59:22 -0300

    (feature):release_develop_branch

---
+++ Game/settings.py
@@ -0,0 +1,16 @@
+'''
+Settings: all game settings.
+'''
+
+# Game settings
+TITLE = 'Develop'
+
+FPS = 30
+WIDTH = 800
+HEIGHT = 600
+BGCOLOR = (255, 255, 255)
+GRIDCOLOR = (0, 0, 0)
+
+TILESIZE = 20
+GRIDWIDTH = WIDTH // TILESIZE
+GRIDHEIGHT = HEIGHT // TILESIZE
