69b0a96086bbb55396a3f5892e9aaa217f00e565
SamuelMarsaro <samarsaro@gmail.com>
2021-12-14 00:07:47 -0300

    (hotfix): monica+bullet

--- Game/Components/monica.py
+++ Game/Components/monica.py
@@ -47,15 +47,6 @@ class Hero:
             self.x = 355
 
         self.win.blit(self.default_monica,(self.x,self.y))
-            
-        x = pg.key.get_pressed()
-        if x[pg.K_a]:
-            monica = pg.image.load(os.path.join(os.path.dirname(__file__), "img/arma_esquerda.png"))
-        elif x[pg.K_d]:
-            monica = pg.image.load(os.path.join(os.path.dirname(__file__), "img/arma.png"))
-        else:
-            monica = pg.image.load(os.path.join(os.path.dirname(__file__), "img/arma_esquerda.png"))
-        self.win.blit(monica, (self.x, self.y))
 
 
 class Bullet:
@@ -66,10 +57,10 @@ class Bullet:
 
     def draw_right(self):
         sansao = pg.image.load(os.path.join(os.path.dirname(__file__), "img/sansao.png"))
-        self.win.blit(sansao, (self.x+50, self.y+10))
+        self.win.blit(sansao, (self.x+50, self.y+50))
         self.x += 20
 
     def draw_left(self):
         sansao = pg.image.load(os.path.join(os.path.dirname(__file__), "img/sansao.png"))
-        self.win.blit(sansao, (self.x-10, self.y+10))
+        self.win.blit(sansao, (self.x-10, self.y+50))
         self.x -= 20
